0b4209b2bb524e2757fb99a57df127b38f693ebe
Sumer <sumerabdalla@hotmail.com>
2021-08-31 11:28:27 -0400

    fnished chaines car

--- PHP - La base/Notes/03 - chaines car.php
+++ PHP - La base/Notes/03 - chaines car.php
@@ -8,11 +8,13 @@
 		<meta charset="utf-8" />
 	</head>
 	<body>
-		<h1>Mot : </h1>
-		<div>Longueur : </div>
-		<div>Position orem : </div>
-		<div>Remplacer : </div>
-		<div>Substring : </div>
+		<h1>Mot : <?= $mot ?> </h1>
+		<div>Longueur : <?= strlen($mot) ?> </div>
+		<div>Position orem : <?= strpos($mot, "orem") ?> </div>
+		<!-- str_replace(old word, new word, from variable) -->
+		<div>Remplacer : <?= str_replace("orem", "ogique", $mot) ?> </div>
+		<!-- a partir de l'index 1, t'en prend 4 -->
+		<div>Substring : <?= substr($mot, 1, 4) ?> </div>
 	</body>
 </html>
 
