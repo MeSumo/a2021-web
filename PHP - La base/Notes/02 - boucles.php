9a649c744ad9faaae36e76aad87e6d7b1f35b845
Sumer <sumerabdalla@hotmail.com>
2021-08-31 11:17:47 -0400

    fnished boucles

--- PHP - La base/Notes/02 - boucles.php
+++ PHP - La base/Notes/02 - boucles.php
@@ -1,3 +1,14 @@
+<?php
+	$tab = []; // ou array()
+	$tab[] = "Hello"; // push
+	$tab[] = "World";
+
+	// affiche "Hello World"
+	foreach($tab as $element){
+		echo $element;
+	}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -6,6 +17,20 @@
 	</head>
 	<body>
 		<h1>Champs radio</h1>
+		<?php
+			for($i = 0; $i < 10; $i++){
+				// fait phpr = contraire de php
+				// -> pour sortir du php pour un moment pour faire html
+				?>
+					<div>
+						<input type="radio" name=""> <?= $i ?>
+					</div>
+				<?php
+				/*echo "<div>";
+				echo "<input type='radio'>" . $i;
+				echo "</div>";*/
+			}
+		?>
 	</body>
 </html>
 
