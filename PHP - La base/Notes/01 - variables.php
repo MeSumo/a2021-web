8ac0b978dafbbd059a1d8223f8b4ec0d2d9d63d0
Sumer <sumerabdalla@hotmail.com>
2021-08-31 10:37:14 -0400

    finished variables

--- PHP - La base/Notes/01 - variables.php
+++ PHP - La base/Notes/01 - variables.php
@@ -1,3 +1,21 @@
+<!--Format pour php 
+	faire phpt pour faire forme php-->
+<?php
+	echo "123";
+	//  ctrl + é -> fait commentaire tout seul 
+	# php prend toute forme de commentaire
+
+	$username = 3; // $ -> variable
+	$username = "John"; // change type selon valeur
+
+	// si comparaison est de deux types different, transforme valeur
+	// pour les rendre meme type
+	if ($username === "John"){ // === verifie valeur et le type! 
+		// do . instead of + to put together
+		// + pour additioner
+		$username = $username . " Smith"; // concactoner
+	}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -5,7 +23,13 @@
 		<meta charset="utf-8" />
 	</head>
 	<body>
-		Nom : 
+		<h1>
+			<!-- faire phpe pour faire affichage d'une variable -->
+			<?= $username ?>
+		</h1>
+		Nom : <?php
+			echo $username;
+		?>
 	</body>
 </html>
 
