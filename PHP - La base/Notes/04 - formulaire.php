0fd629ca72ebdad2dcbbcbe94d77df9b8704416a
Sumer <sumerabdalla@hotmail.com>
2021-08-31 12:04:29 -0400

    finished formulaire

--- PHP - La base/Notes/04 - formulaire.php
+++ PHP - La base/Notes/04 - formulaire.php
@@ -5,7 +5,18 @@
 		<meta charset="utf-8" />
 	</head>
 	<body>
-		<form action="04 - formulaire.php" method="get">
+		<?php
+			// ou $_POST["..."] si on est en method="post"
+			//if(isset($_GET["info"]) && strlen($_GET["info"]) > 0){
+				// ou (les deux cherches si y a quelque chose dans info)
+			if(!empty($_GET["info"])){
+				?>
+					<div style="color:blue"> Heyooooo </div>
+				<?php
+			}
+		?>
+		<!-- get montre resultat dans le lien, tandis que post cache -->
+		<form action="04 - formulaire.php" method="get"> 
 			<div>
 				Info : <input type="text" name="info" />
 			</div>
