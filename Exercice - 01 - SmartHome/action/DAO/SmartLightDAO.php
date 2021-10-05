6b65d2f6b85c8a9302bb21d6c58cb79c117d3b15
Sumer <sumerabdalla@hotmail.com>
2021-10-05 09:56:12 -0400

    smart home donee db

---
+++ Exercice - 01 - SmartHome/action/DAO/SmartLightDAO.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once("action/DAO/Connection.php");
+
+    class SmartLightDAO {
+
+        public static function getLightsStatus() {
+            $connection = Connection::getConnection();
+            $result = [];
+
+            $statement = $connection->prepare("SELECT * FROM lights");
+            $statement->setFetchMode(PDO::FETCH_ASSOC);
+            $statement->execute();
+
+            while ($row = $statement->fetch()){
+                $result[] = [$row["name"], $row["status"]];
+            }
+
+            return $result;
+        }
+
+    }
