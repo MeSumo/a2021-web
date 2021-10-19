252f78ef6edac07f373a9d939b9959f56f5c1fdf
Sumer <sumerabdalla@hotmail.com>
2021-10-19 09:59:46 -0400

    ajax smart home done

--- AJAX/03 - Exercice - SmartHome/js/javascript.js
+++ AJAX/03 - Exercice - SmartHome/js/javascript.js
@@ -1,5 +1,20 @@
+function updateLights() {
+    fetch("ajax.php")
+    .then(response => response.json())
+    .then(response => {
+        //console.log(response);
+        response.forEach(element => {
+            var light = element["name"];
+            if (element["status"] === 1){
+                document.querySelector("#"+light).src = "img/light-on.png";
+            }
+            else{
+                document.querySelector("#"+light).src = "img/light-off.png";
+            }
+        })
+    })
+}
+
 window.addEventListener("load", () => {
-    document.querySelector(".btn-refresh").onclick = () => {
-        window.location.href = "index.php";
-    }
+    setInterval(updateLights, 5000);
 })
